fa1f1d5dcf9b8902252c8f85a82bb2dabea6c5f5
asrebel <asrebel777@gmail.com>
2024-01-05 19:42:40 +0500

    first video

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
+
+function App() {
+    return (
+        <>
+            <Header titleForHeader = {'New Header'} />
+            <Body titleForBody = {'New Body'}/>
+            <Footer titleForFooter = {'New Footer'}/>
+        </>
+    );
+}
+
+export default App;
